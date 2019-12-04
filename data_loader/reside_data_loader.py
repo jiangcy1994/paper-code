4c2e720c5374fb7542d5e5758c43092053225b8f
江春源 <jiangcy1994@users.noreply.github.com>
2019-12-04 14:52:39 +0800

    resturct dataloader

--- data_loader/reside_data_loader.py
+++ data_loader/reside_data_loader.py
@@ -1,162 +1,35 @@
-import cv2
-from glob import glob
+from data_loader import DataLoader
+import tensorflow as tf
 import numpy as np
-import os
 
-class RESIDEOTSDataLoader():
-    def __init__(self, img_res=(256, 256), 
-                 clear_path='D:/DataSet/RESIDE/OTS_ALPHA/clear/clear_images/', 
-                 haze_path='D:/DataSet/RESIDE/OTS_ALPHA/haze/OTS/'):
+
+class RESIDEOTSDataLoader(DataLoader):
+    def __init__(self,
+                 img_shape=(256, 256),
+                 clear_path='D:DataSet/RESIDE/OTS_ALPHA/clear_sl/',
+                 haze_path='D:DataSet/RESIDE/OTS_ALPHA/haze/',
+                 train_path='RESIDE_OTS_train.txt',
+                 test_path='RESIDE_OTS_test.txt'):
+        super(RESIDEOTSDataLoader, self).__init__(img_shape)
         self.dataset_name = self.__class__
-        self.img_res = img_res
-        
-        self.clear_path = clear_path
-        self.haze_path = haze_path
+        self.clear_path, self.haze_path = clear_path, haze_path
+        self.train_path, self.test_path = train_path, test_path
         self.img_type = '.jpg'
-        self.to_clear_base_name = lambda x: x.split('_')[0] + '.jpg'
-        self.train_range = range(1, 5400)
-        self.valid_range = range(5401, 7200)
-        self.test_range = range(7201, 10000)
-        
-    def load_data(self, batch_size=1, is_testing=False):
-        haze_base_names = [os.path.basename(path) for path in glob(self.haze_path + '*' + self.img_type)]
-        if is_testing:
-            haze_base_names = [*filter(lambda x: int(x.split('_')[0]) in self.test_range, haze_base_names)]
-        else:
-            haze_base_names = [*filter(lambda x: int(x.split('_')[0]) not in self.test_range, haze_base_names)]     
-        
-        batch_haze_base_names = np.random.choice(haze_base_names, size=batch_size)
 
-        imgs = []
-        for haze_base_name in batch_haze_base_names:
-            img = self.load_img(self.haze_path + haze_base_name)
-            if not is_testing and np.random.random() > 0.5:
-                img = np.fliplr(img)
-            imgs.append(img)
-        imgs = np.array(imgs)
-        return imgs
+    def train_data(self):
+        basename_ds = tf.data.Dataset.from_tensor_slices(
+            np.loadtxt(self.train_path, dtype=str))
 
-    def load_batch(self, batch_size=1, is_testing=False):
-        haze_base_names = [os.path.basename(path) for path in glob(self.haze_path + '*' + self.img_type)]
-        
-        if is_testing:
-            haze_base_names = [*filter(lambda x: int(x.split('_')[0]) in self.valid_range, haze_base_names)]
-        else:
-            haze_base_names = [*filter(lambda x: int(x.split('_')[0]) in self.train_range, haze_base_names)]
+        haze_train_img_ds = self._get_img_ds(basename_ds, self.haze_path)
+        clear_train_img_ds = self._get_img_ds(basename_ds, self.clear_path)
 
-        self.n_batches = int(len(haze_base_names) / batch_size)
-        total_samples = self.n_batches * batch_size
+        return haze_train_img_ds, clear_train_img_ds
 
-        # Sample n_batches * batch_size from each path list so that model sees all
-        # samples from both domains
-        haze_base_names = np.random.choice(haze_base_names, total_samples, replace=False)
+    def test_data(self):
+        basename_ds = tf.data.Dataset.from_tensor_slices(
+            np.loadtxt(self.test_path, dtype=str))
 
-        for i in range(self.n_batches-1):
-            batch_haze = haze_base_names[i*batch_size : (i+1)*batch_size]
-            clear_imgs, haze_imgs = [], []
-            for haze_base_name in batch_haze:
-                clear_img = self.load_img(self.clear_path + self.to_clear_base_name(haze_base_name))
-                haze_img = self.load_img(self.haze_path + haze_base_name)
+        haze_train_img_ds = self._get_img_ds(basename_ds, self.haze_path)
+        clear_train_img_ds = self._get_img_ds(basename_ds, self.clear_path)
 
-                if not is_testing and np.random.random() > 0.5:
-                    clear_img = np.fliplr(clear_img)
-                    haze_img = np.fliplr(haze_img)
-
-                clear_imgs.append(clear_img)
-                haze_imgs.append(haze_img)
-
-            clear_imgs = np.array(clear_imgs)
-            haze_imgs = np.array(haze_imgs)
-
-            yield clear_imgs, haze_imgs
-
-    def load_img(self, path):
-        img = self.imread(path)
-        img = cv2.resize(img, self.img_res)
-        img = img / 127.5 - 1.
-        return img[:, :, :]
-
-    def imread(self, path):
-        return cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2RGB).astype(np.float)
-
-
-class RESIDEOTSDiscriminatorDataLoader():
-    def __init__(self, img_res=(256, 256),
-                 clear_path='D:/DataSet/RESIDE/OTS_ALPHA/clear/clear_images/', 
-                 haze_path='D:/DataSet/RESIDE/OTS_ALPHA/haze/OTS/'):
-        self.dataset_name = self.__class__
-        self.img_res = img_res
-        
-        self.clear_path = clear_path
-        self.haze_path = haze_path
-        self.img_type = '.jpg'
-        self.to_clear_base_name = lambda x: x.split('_')[0] + '.jpg'
-        self.train_range = range(1, 5400)
-        self.valid_range = range(5401, 7200)
-        self.test_range = range(7201, 10000)
-        
-    def load_data(self, batch_size=1, is_testing=False):
-        haze_base_names = [os.path.basename(path) for path in glob(self.haze_path + '*' + self.img_type)]
-        if is_testing:
-            haze_base_names = [*filter(lambda x: int(x.split('_')[0]) in self.test_range, haze_base_names)]
-        else:
-            haze_base_names = [*filter(lambda x: int(x.split('_')[0]) not in self.test_range, haze_base_names)]     
-        
-        batch_haze_base_names = np.random.choice(haze_base_names, size=batch_size)
-
-        imgs = []
-        for haze_base_name in batch_haze_base_names:
-            img = self.load_img(self.haze_path + haze_base_name)
-            if not is_testing and np.random.random() > 0.5:
-                img = np.fliplr(img)
-            imgs.append(img)
-        imgs = np.array(imgs)
-        return imgs
-
-    def load_batch(self, batch_size=2, is_testing=False):
-        haze_base_names = [os.path.basename(path) for path in glob(self.haze_path + '*' + self.img_type)]
-        
-        if is_testing:
-            haze_base_names = [*filter(lambda x: int(x.split('_')[0]) in self.valid_range, haze_base_names)]
-        else:
-            haze_base_names = [*filter(lambda x: int(x.split('_')[0]) in self.train_range, haze_base_names)]     
-
-        self.n_batches = int(len(haze_base_names) / batch_size)
-        total_samples = self.n_batches * batch_size
-
-        # Sample n_batches * batch_size from each path list so that model sees all
-        # samples from both domains
-        haze_base_names = np.random.choice(haze_base_names, total_samples, replace=False)
-
-        for i in range(self.n_batches-1):
-            batch_haze = haze_base_names[i*batch_size : (i+1)*batch_size]
-            clear_imgs, haze_imgs = [], []
-            imgs, results = [], []
-            for haze_base_name in batch_haze:
-                clear_img = self.load_img(self.clear_path + self.to_clear_base_name(haze_base_name))
-                haze_img = self.load_img(self.haze_path + haze_base_name)
-
-                if not is_testing and np.random.random() > 0.5:
-                    clear_img = np.fliplr(clear_img)
-                    haze_img = np.fliplr(haze_img)
-
-                clear_imgs.append(clear_img)
-                haze_imgs.append(haze_img)
-            
-            imgs = np.concatenate([np.array(clear_imgs), 
-                                   np.array(haze_imgs)])
-            results = np.concatenate([np.ones((len(batch_haze), 16, 16, 1)),
-                                      np.zeros((len(batch_haze), 16, 16, 1))])
-
-            yield imgs, results
-
-    def load_img(self, path):
-        img = self.imread(path)
-        img = cv2.resize(img, self.img_res)
-        img = img / 127.5 - 1.
-        return img[:, :, :]
-
-    def imread(self, path):
-        return cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2RGB).astype(np.float)
-
-    
+        return haze_train_img_ds, clear_train_img_ds
--- utils/image.py
+++ utils/image.py
@@ -1,11 +1,13 @@
 import tensorflow as tf
 
+
 def preprocess_image(image, img_shape):
     image = tf.image.decode_jpeg(image, channels=3)
-    image = tf.image.resize(image, [192, 192])
+    image = tf.image.resize(image, img_shape)
     image /= 255.0
 
     return image
+
 
 def load_and_preprocess_image(path, img_shape):
     image = tf.io.read_file(path)
