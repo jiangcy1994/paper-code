98cf120929f52cd9b712dec21c50729f5bf2ca23
江春源 <jiangcy1994@users.noreply.github.com>
2019-12-07 17:02:55 +0800

    Update reside_data_loader.py

--- data_loader/reside_data_loader.py
+++ data_loader/reside_data_loader.py
@@ -17,34 +17,57 @@ class RESIDEOTSDataLoader(DataLoader):
         self.clear_path, self.depth_path, self.haze_path = clear_path, depth_path, haze_path
         self.train_path, self.test_path = train_path, test_path
         self.img_type = '.jpg'
+        h5py.get_config().default_file_mode = 'r'
 
-    def _get_atmos_ds(ds):
-        atmos_ds = ds.map(map_func=lambda x: x.split('_')[1])
-        return trans_ds
+    def _gen_gen(self, data_list, gen_func):
+        def gen():
+            for data in data_list:
+                yield gen_func(data)
+        return gen
 
-    def _get_trans_ds(ds):
-        beta_ds = ds.map(map_func=lambda x: x.split('_')[2])
-        depth_path_ds = ds.map(
-            map_func=lambda x: self.depth_path + x.split('_')[0] + '.mat')
-        depth_ds = ds.map(map_func=lambda x: h5py.File(
-            filename, 'r')['depth'][:].transpose())
-        trans_ds = tf.data.Dataset.zip((beta_ds, depth_ds))
-        trans_ds = trans_ds.map(map_func=lambda beta,
-                                depth: tf.exp(-1 * beta * depth))
-        return trans_ds
+    def _get_atmos_ds(self, base_name_list):
+
+        def _gen_atmos(base_name):
+            return tf.fill([self.img_shape[0], self.img_shape[1], 1], float(base_name.split('_')[1]))
+
+        return tf.data.Dataset.from_generator(
+            self._gen_gen(base_name_list, _gen_atmos),
+            tf.float32,
+            tf.TensorShape([self.img_shape[0], self.img_shape[1], 1])
+        )
+
+    def _get_trans_ds(self, base_name_list):
+
+        def _get_trans(base_name):
+            beta = float(base_name.split('_')[2][:-4])
+            depth = tf.keras.utils.HDF5Matrix(
+                self.depth_path + base_name.split('_')[0] + '.mat', 'depth')
+            depth = tf.image.resize(
+                tf.expand_dims(depth, axis=-1),
+                self.img_shape
+            )
+            trans = tf.exp(-1 * beta * depth)
+            trans = tf.concat([trans, trans, trans], axis=-1)
+            return trans
+
+        return tf.data.Dataset.from_generator(
+            self._gen_gen(base_name_list, _get_trans),
+            tf.float32,
+            tf.TensorShape([self.img_shape[0], self.img_shape[1], 3])
+        )
 
     def train_data(self, include_trans_atmos=False):
-        basename_ds = tf.data.Dataset.from_tensor_slices(
-            np.loadtxt(self.train_path, dtype=str))
+        basename_list = np.loadtxt(self.train_path, dtype=str)
+        basename_ds = tf.data.Dataset.from_tensor_slices(basename_list)
 
         if include_trans_atmos:
             haze_train_img_ds = self._get_img_ds(basename_ds, self.haze_path)
             clear_train_img_ds = self._get_img_ds(basename_ds, self.clear_path)
-            trans_ds = self._get_trans_ds(basename_ds)
-            atmos_ds = self._get_atmos_ds(basename_ds)
-            
+            trans_ds = self._get_trans_ds(basename_list)
+            atmos_ds = self._get_atmos_ds(basename_list)
+
             return haze_train_img_ds, clear_train_img_ds, trans_ds, atmos_ds
-            
+
         else:
             haze_train_img_ds = self._get_img_ds(basename_ds, self.haze_path)
             clear_train_img_ds = self._get_img_ds(basename_ds, self.clear_path)
