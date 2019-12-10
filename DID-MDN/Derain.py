66a97119b79aeede5999f96f653d2d14a5f7d51d
江春源 <jiangcy1994@users.noreply.github.com>
2019-12-10 22:12:16 +0800

    Update Derain.py

--- DID-MDN/Derain.py
+++ DID-MDN/Derain.py
@@ -1,110 +1,100 @@
 import datetime
-from tensorflow.keras.layers import Input, Subtract
-from tensorflow.keras.models import Model
+import tensorflow as tf
+from tensorflow.keras.optimizers import Adam
 import numpy as np
-import os
 from utils import *
 
 __all__ = ['Derain']
+
 
 class Derain:
     '''
     Training (Density-aware Deraining network using GT label)
     derain_train_2018.py
     '''
-    
-    def __init__(self, img_shape=(512, 512, 3), lambda_F=1.8):
-        # Input shape
+
+    def __init__(self, img_shape=(512, 512, 3), lambda_F=1):
+
         self.img_rows, self.img_cols, self.channels = self.img_shape = img_shape
         self.label_shape = img_shape[:2] + (8,)
         self.lambda_F = lambda_F
 
         self.generator = self.build_generator()
-        self.generator.name = 'generator'
-        self.vgg16_feature = vgg16_feature_net(img_shape)
-        
-        img_input = Input(img_shape, name='img_input')
-        label_input = Input(self.label_shape, name='label_input')
-        
-        residue, x_hat = self.generator([img_input, label_input])
-        vgg16_feature = self.vgg16_feature(x_hat)
-        
-        self.combined = Model(inputs=[
-                                  # input image
-                                  img_input,
-                                  # input label
-                                  label_input],
-                              outputs=[
-                                  # L_{E,r} not found
-                                  # L_{E,d}
-                                  x_hat, 
-                                  # L_F
-                                  # actually only vgg16_feature[0] is written in the artical
-                                  vgg16_feature[0], vgg16_feature[1]
-                              ])
-        self.combined.compile('rmsprop',
-                              loss=['mse', 'mse', 'mse'], 
-                              loss_weights=[1, self.lambda_F, self.lambda_F])
-        
-        
+        self.generator_optimizer = Adam(1e-3, beta_1=0.5)
+
+        self.vgg16_extract_feature = vgg16_feature_net(img_shape)
+        self.vgg16_extract_feature.trainable = False
+
+        checkpoint_path = "./checkpoints/train"
+
+        ckpt = tf.train.Checkpoint(generator=self.generator,
+                                   generator_optimizer=self.generator_optimizer)
+
+        self.ckpt_manager = tf.train.CheckpointManager(
+            ckpt, checkpoint_path, max_to_keep=5)
+
+        if self.ckpt_manager.latest_checkpoint:
+            ckpt.restore(self.ckpt_manager.latest_checkpoint)
+            print('Latest checkpoint restored!!')
+
     def build_generator(self):
         return Dense_rain(self.img_shape)
-    
-    def train(self, data_loader, epochs, batch_size=1, sample_interval=50):
 
-        start_time = datetime.datetime.now()
-        total_loss = 0
-        l2_loss = 0
-        feature_loss0 = 0
-        feature_loss1 = 0
-        
+    def euclidean_derain_loss(self, real_target, fake_target):
+        return tf.reduce_mean(tf.square(real_target - fake_target))
+
+    def feature_based_loss(self, real_target, fake_target):
+        real_trans_feature = self.vgg16_extract_feature(real_target)
+        generated_trans_feature = self.vgg16_extract_feature(fake_target)
+        loss = tf.reduce_mean(tf.square(real_trans_feature[0] - generated_trans_feature[0])) + tf.reduce_mean(
+            tf.square(real_trans_feature[1] - generated_trans_feature[1]))
+        return self.lambda_F * loss
+
+    @tf.function
+    def train_step(self, image, label, target):
+        with tf.GradientTape(persistent=True) as tape:
+
+            fake_residue, fake_target = self.generator(
+                [image, label], training=True)
+            real_residue = tf.subtract(target, label)
+
+            l_ed = self.euclidean_derain_loss(target, fake_target)
+            l_f = self.feature_based_loss(target, fake_target)
+
+            total_gen_loss = l_er + l_ed + l_f
+
+        generator_gradients = tape.gradient(
+            total_gen_loss, self.generator.trainable_variables)
+
+        self.generator_optimizer.apply_gradients(
+            zip(generator_gradients, self.generator.trainable_variables))
+        return total_gen_loss
+
+    def train(self, epochs, dataset_image, dataset_label, dataset_target,  batch_size, loss_interval):
+
         for epoch in range(epochs):
-            for batch_i, (img_input, target_input, label) in enumerate(data_loader.load_batch(batch_size)):
-                vgg16_feature = self.vgg16_feature.predict(target_input)
-                label_input = np.zeros((label.shape[0],) + self.label_shape)
-                for i, one_label in enumerate(label_input):
-                    one_label.fill(label[i])
-                
-                loss = self.combined.train_on_batch(
-                    [img_input, label_input],
-                    [target_input, vgg16_feature[0], vgg16_feature[1]]
-                )
-               
-                # Plot the progress
-                if batch_i % sample_interval == 0:
-                    elapsed_time = datetime.datetime.now() - start_time
-                    total_loss = np.sum(loss)
-                    l2_loss = loss[0]
-                    feature_loss0 = loss[1]
-                    feature_loss1 = loss[2]
-                    print("[Epoch %d/%d] [Batch %d/%d] [loss: %f, l2: %f, features: (%f, %f)] time: %s " %
-                          (epoch, epochs,
-                           batch_i, data_loader.n_batches * 3,
-                           total_loss, l2_loss, feature_loss0, feature_loss1, 
-                           elapsed_time))
-        
-        elapsed_time = datetime.datetime.now() - start_time
-        # Print Result After Train
-        print("[loss: %f, l2: %f, features: (%f, %f)] time: %s " %
-              (total_loss, l2_loss, feature_loss0, feature_loss1, 
-               elapsed_time))
-    
-    def predict(self, img_input, label):
-        label_input = np.zeros((label.shape[0],) + self.label_shape)
-        for i, one_label in enumerate(label_input):
-            one_label.fill(label[i])
-        result = self.generator.predict([img_input, label_input])
-        return result
-    
-    def save_model(self, dir_name='model'):
-        if not os.path.exists(dir_name) and not os.path.isdir(dir_name):
-            os.mkdir(dir_name)
-            
-        self.generator.save_weights(dir_name + '/derain.hdf5')
-        
-    def load_model(self, dir_name='model'):
-        file_name = dir_name + '/derain.hdf5'
-        if os.path.exists(file_name) and os.path.isfile(file_name):
-            self.generator.load_model(file_name)
-        else:
-            print('no model weights file in {0}'.format(file_name))
+            start = datetime.datetime.now()
+
+            n = 0
+            print('epoch {0}/{1}'.format(epoch + 1, epochs))
+            for image, label, target in tf.data.Dataset.zip((dataset_image, dataset_label, dataset_target)).batch(batch_size):
+                total_gen_loss = self.train_step(image, label, target)
+                if n % loss_interval == 0:
+                    print('Batch:{0} | Loss: | G: {1}'.format(
+                        n, total_gen_loss))
+                n += 1
+
+            ckpt_save_path = self.ckpt_manager.save()
+            print('Saving checkpoint for epoch {} at {}'.format(epoch+1,
+                                                                ckpt_save_path))
+
+            print('Time taken for epoch {} of totoal epoch {} is {}\n'.format(
+                epoch + 1,
+                epochs,
+                datetime.datetime.now() - start))
+
+        ckpt_save_path = self.ckpt_manager.save()
+        print('Saving checkpoint for epoch {} at {}'.format(
+            epoch+1,
+            ckpt_save_path))
+        print('Time taken is {}\n'.format(datetime.datetime.now() - start))
